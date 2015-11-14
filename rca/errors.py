c0e874b770adabc96b88bbfc16a67f9e69419cbc
vanzhiganov <vanzhiganov@ya.ru>
2015-11-14 23:56:04 +0300

    errors

---
+++ rca/errors.py
@@ -0,0 +1,19 @@
+MISSING_PARAMETER_URL = {
+    "error_type": "external",
+    "url": "requested url",
+    "error_message": "'url' parameter is missing"
+}
+INVALID_PARAMETER_URL = {
+    "error_type": "external",
+    "url": "requested url",
+    "error_message": "Invalid 'url' parameter"
+}
+INVALID_PARAMETER_CALLBACK = {
+    "error_type": "internal",
+    "url": "requested url",
+    "error_message": "Invalid 'callback' parameter"
+}
+RESOURCE_NOT_AVAILABLE = {
+    "error_type": "internal",
+    "error_message": "Defined 'url' not available"
+}
